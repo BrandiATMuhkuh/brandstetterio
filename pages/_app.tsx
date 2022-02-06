f3358d8f6dc1a0b7f3106374a17eaf2016020ad6
Jürgen Brandstetter <jurgen@jaipuna.com>
2022-02-06 21:08:25 +0100

    add materila ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "../components/theme";
+import AppBar from "../components/AppBar";
+import { Box, Container, Paper } from "@mui/material";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <AppBar />
+        <Container maxWidth="md">
+          <Box pt={1}>
+            <Paper variant="outlined">
+              <Box m={1}>
+                <Component {...pageProps} />
+              </Box>
+            </Paper>
+          </Box>
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/educations/index.tsx
+++ pages/educations/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 
 export default function index() {
   return (
     <>
       <h1>educations</h1>
-      The index of all education
+      {new Array(100).fill(1).map((e, i) => (
+        <p key={i}>
+          The index of all education asdf f asfsda fdsa f afdsafsfdsaf asfd saf
+          asf asfasdfas asfdsafs
+        </p>
+      ))}
     </>
   );
 }
